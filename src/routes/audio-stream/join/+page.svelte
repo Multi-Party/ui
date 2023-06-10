75c5bedf95a6c5b3671a680857c7023ca55d7721
Vojtech Simetka <vojtech@simetka.cz>
2023-06-10 19:43:01 +0200

    style: join screen

--- src/routes/audio-stream/join/+page.svelte
+++ src/routes/audio-stream/join/+page.svelte
@@ -6,17 +6,41 @@
 	let addressOrEns = ''
 </script>
 
-<h3>Or search on ENS or Ethereum address</h3>
-<input bind:value={addressOrEns} />
-<Button disabled={!addressOrEns} on:click={() => goto(AUDIO_STREAM_CONNECT(addressOrEns))}
-	>Start listening</Button
->
+<div class="flex">
+	<h3>Search ENS or Ethereum address</h3>
+</div>
+<div class="buttons">
+	<input bind:value={addressOrEns} />
+	<Button disabled={!addressOrEns} on:click={() => goto(AUDIO_STREAM_CONNECT(addressOrEns))}
+		>Start listening</Button
+	>
+</div>
 
 <style>
 	input {
 		background: var(--white);
 		border: 2px solid var(--black);
 		border-radius: 7px;
-		padding: 15px;
+		padding: 5px;
+		font-family: 'Syne';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 25px;
+		line-height: 38px;
+		text-align: center;
+	}
+	.flex {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		gap: 22px;
+		flex-grow: 1;
+	}
+	.buttons {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 22px;
+		flex-grow: 1;
 	}
 </style>
